8d1a701a4e05da4486f73c4bfeaace4e8ee9b248
2over12 <smith.ian@husky.neu.edu>
2021-01-12 11:06:48 -0500

    implemented simple full analysis server

--- infer/src/InferServer.ml
+++ infer/src/InferServer.ml
@@ -1,14 +1,23 @@
 open! IStd
 open Async
 
+module GhidraNP: JsonAnalyze.NameParsers = struct 
+    let lang = Language.Clang
 
+    let parse_procname _ = raise (Failure "proc name not implemented")
+    let fieldname_of_string _ = raise (Failure "fieldname parse not implemented")
+
+    let typename_of_classname _ = raise (Failure "classname parse not implemented")
+end
+
+module GhidraAnalyze = JsonAnalyze.NameParsersToAnalyzer(GhidraNP)
 
 let port_param = let open Command.Param in anon ("port" %: int)
 
 
-let read_single_message = let parse_message = GhidraATD.Functions_j.ghidra_function_of_string in (fun rdr -> GhidraATD.Framing.t_from_reader rdr parse_message)
+let read_single_message = let parse_message = GhidraATD.Functions_j.json_analyze_req_of_string in (fun rdr -> GhidraATD.Framing.t_from_reader rdr parse_message)
 
-let read_and_print_everyting _addr (r:Reader.t) _w = let message_pipe = Reader.read_all r read_single_message in Pipe.iter message_pipe ~f:(fun (msg:GhidraATD.Functions_t.ghidra_function) -> msg.source |> print_endline |> return)
+let read_and_print_everyting _addr (r:Reader.t) _w = let message_pipe = Reader.read_all r read_single_message in Pipe.iter message_pipe ~f:(fun (msg:GhidraATD.Functions_t.json_analyze_req) -> GhidraAnalyze.analyze_json msg.cfg msg.tenv |> return)
 
 let run port =
   let%bind server =
